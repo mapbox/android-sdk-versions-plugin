12aba5b816666ed3a1697717b4ba7d6b939fe25e
Harsha Sura <harsha.sura@mapbox.com>
2020-06-17 16:59:08 -0700

    Access the lazy provider(generateBuildConfigProvider.get().versionCode.get()) method via get() method.

--- src/main/groovy/com/mapbox/android/sdk/versions/PersistSDKVersionInfo.groovy
+++ src/main/groovy/com/mapbox/android/sdk/versions/PersistSDKVersionInfo.groovy
@@ -35,7 +35,7 @@ class PersistSDKVersionInfo implements Plugin<Project> {
                 def outputFile = new File(sdkVersionsDir, variant.applicationId)
                 saveSDKVersionTask.outputFile = outputFile
                 saveSDKVersionTask.sdkVersion = project.version
-                saveSDKVersionTask.sdkVersionCode = variant.generateBuildConfigProvider.get().versionCode
+                saveSDKVersionTask.sdkVersionCode = variant.generateBuildConfigProvider.get().versionCode.get()
 
                 if (!validateVersion(saveSDKVersionTask.sdkVersion)) {
                     throw new IllegalStateException("Version $saveSDKVersionTask.sdkVersion invalid" +
